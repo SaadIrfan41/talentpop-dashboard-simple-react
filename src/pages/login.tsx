02a0a919e41511e4335e21015a838c716e117b1e
saix511 <saadirfan41@gmail.com>
2023-08-06 20:54:18 +0500

    .

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,18 +1,13 @@
-import Logo from "@/assets/logo.png";
+import Logo from '@/assets/logo.png'
 
-import LoginForm from "@/components/Forms/LoginForm";
+import LoginForm from '@/components/Forms/LoginForm'
 
 const Login = () => {
   return (
-    <div className=" mx-auto flex h-screen max-w-7xl flex-col items-center justify-center ">
-      <img
-      
-        src={Logo}
-        alt="TalentPop"
-        className=" -ml-3 mb-6 object-contain"
-      />
+    <div className=' mx-auto flex h-screen max-w-7xl flex-col items-center justify-center '>
+      <img src={Logo} alt='TalentPop' className=' -ml-3  object-contain' />
 
-      <h1 className=" mx-auto mb-5 text-4xl font-black text-[#69C920] text-opacity-80">
+      <h1 className=' mx-auto mb-5 text-4xl font-black text-[#69C920] text-opacity-80'>
         Log in
       </h1>
       <LoginForm />
@@ -21,7 +16,7 @@ const Login = () => {
         Forgot password?
       </span> */}
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
